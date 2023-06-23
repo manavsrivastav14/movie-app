916b5df8efe0019c9166c96db0904b9f14b1c459
Manav Srivastava <manavsrivastav14@gmail.com>
2023-06-23 17:59:16 +0530

    Dispatching action

--- src/components/App.js
+++ src/components/App.js
@@ -14,8 +14,8 @@ function App() {
         </div>
 
         <div className="List">
-          {data.map((movie) => (
-            <MovieCard movie={movie} />
+          {data.map((movie, index) => (
+            <MovieCard movie={movie} key={`movies-${index}`} />
           ))}
         </div>
       </div>
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
-import { configureStore, createStore } from "@reduxjs/toolkit";
+import { createStore } from "redux";
 import movies from "./reducers";
 
 // const store = configureStore({
@@ -12,7 +12,14 @@ import movies from "./reducers";
 const store = createStore(movies);
 
 console.log("Store:", store);
-console.log("Store state:", store.getState());
+console.log("Before state:", store.getState());
+
+store.dispatch({
+  type: "ADD_MOVIES",
+  movies: [{ name: "Superman" }],
+});
+
+console.log("After state:", store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
