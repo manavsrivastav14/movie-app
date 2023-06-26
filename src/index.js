92ac58b1bacbf81b752f3f7a4e3471f8b2d03295
Manav Srivastava <manavsrivastav14@gmail.com>
2023-06-26 11:31:02 +0530

    Modifying middleware funtions to arrow functions

--- src/index.js
+++ src/index.js
@@ -8,16 +8,25 @@ import combineReducers from "./reducers";
 // used for console logging the action types
 
 // function logger(obj, next, action)
-const logger = function ({ dispatch, getState }) {
-  //the object consits of 2 properties a. dispatch property, b. getState property. Both the functions are present in store already. The redux will automatically pass those functions here inside argument object's properties.
-  return function (next) {
-    return function (action) {
-      // Middleware code
-      console.log("ACTION_TYPE: ", action.type);
-      next(action); // next is used to call next middleware function. If there is no other middleware left then instead of next automatically dispatch funtion will be called.
-    };
+// const logger = function ({ dispatch, getState }) {
+//   //the object consits of 2 properties a. dispatch property, b. getState property. Both the functions are present in store already. The redux will automatically pass those functions here inside argument object's properties.
+//   return function (next) {
+//     return function (action) {
+//       // Middleware code
+//       console.log("ACTION_TYPE: ", action.type);
+//       next(action); // next is used to call next middleware function. If there is no other middleware left then instead of next automatically dispatch funtion will be called.
+//     };
+//   };
+// };
+
+// Other way of writing the above function
+const logger =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log("ACTION_TYPE: ", action.type);
+    next(action);
   };
-};
 
 const store = createStore(combineReducers, applyMiddleware(logger));
 
